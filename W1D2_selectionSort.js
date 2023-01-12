2028eac288f364e428db209a0b730412aa6fb222
Robert Lutrick <r.lutrick@icloud.com>
2023-01-11 20:13:46 -0600

    First Commit

---
+++ W1D2_selectionSort.js
@@ -0,0 +1,66 @@
+/* 
+    https://visualgo.net/en/sorting
+        
+    Selection sort works by iterating through the list, finding the minimum
+    value, and moving it to the beginning of the list. Then, ignoring the first
+    position, which is now sorted, iterate through the list again to find the
+    next minimum value and move it to index 1. This continues until all values
+    are sorted.
+    Unstable sort.
+    
+    Time Complexity
+        - Best: O(n^2) quadratic.
+        - Average: O(n^2) quadratic.
+        - Worst: O(n^2) quadratic.
+    Space: O(1) constant.
+    Selection sort is one of the slower sorts.
+    - ideal for: pagination, where page 1 displays 10 records for example,
+        you run selection sort for 10 iterations only to display the first 10
+        sorted items.
+*/
+
+// const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+// const numsReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+// const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ * Sorts given array in-place.
+ * Best: O(n^2) quadratic.
+ * Average: O(n^2) quadratic.
+ * Worst: O(n^2) quadratic.
+ * @param {Array<number>} nums
+ * @returns {Array<number>} The given array after being sorted.
+ */
+
+/*****************************************************************************/
+
+/**
+ * Sorts given array in-place.
+ * Best: O(n^2) quadratic.
+ * Average: O(n^2) quadratic.
+ * Worst: O(n^2) quadratic.
+ * @param   {Array<number>} nums
+ * @return  {Array<number>} The given array after being sorted.
+ */
+const selectionSort = (nums = []) => {
+    for (let i = 0; i < nums.length; i++) {
+        let min_index = i;
+        for (let j = i + 1; j < nums.length; j++) {
+            let comare_index = j;
+            if (nums[comare_index] < nums[min_index]) {
+                min_index = comare_index;
+            }
+        }
+        // Swap current index value with new min index value
+        [nums[i], nums[min_index]] = [nums[min_index], nums[i]];
+    }
+    return nums
+}
+const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+const numsReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(selectionSort(numsRandomOrder));
+
+module.exports = { selectionSort };
---
+++ median_two_sorted_arrays.js
@@ -0,0 +1,50 @@
+/*
+Given two sorted arrays nums1 and nums2 of size m and n respectively, 
+return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+*/
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+
+// nums1 = [1, 2]
+// nums2 = [3, 4]
+nums1 = [3]
+nums2 = [-2, -1]
+
+const findMedianSortedArrays = (nums1, nums2) => {
+    let m = nums1.length
+    let n = nums2.length
+
+    // median = 0
+
+    // for (let i = 0; i < m; i++) {
+    //     median += nums1[i]
+    // }
+
+    // for (let i = 0; i < n; i++) {
+    //     median += nums2[i]
+    // }
+
+    // return median / (m + n)
+
+    for (let i = 0; i < n; i++) {
+        nums1.push(nums2[i])
+    }
+
+    nums1 = nums1.sort(function (a, b) { return a - b })
+    console.log(nums1)
+    if (m % 2 == 0) {
+        val1 = nums1[Math.floor(m / 2)]
+        val2 = nums1[Math.floor(m / 2) - 1]
+        return (val1 + val2) / 2
+    } else {
+        return nums1[Math.floor(m / 2)]
+    }
+};
+
+console.log(findMedianSortedArrays(nums1, nums2))
