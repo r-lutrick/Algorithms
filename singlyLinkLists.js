f4dd1d911a9b7e7ea16e28d2ba2daf4a1b72f212
Robert Lutrick <r.lutrick@icloud.com>
2023-02-09 11:55:56 -0600

    Additional functionality

--- singlyLinkLists.js
+++ singlyLinkLists.js
@@ -30,11 +30,11 @@ class ListNode {
  * functionality (methods) that each list should have.
  */
 class SinglyLinkedList {
+
     /**
-     * Constructs a new instance of an empty linked list that inherits all the
-     * methods.
+     * Constructs a new instance of an empty linked list that inherits all the methods.
      * @returns {SinglyLinkedList} The new list that is instantiated is implicitly
-     *    returned without having to explicitly write "return".
+     *  returned without having to explicitly write "return".
      */
     constructor() {
         /** @type {ListNode|null} */
@@ -43,8 +43,6 @@ class SinglyLinkedList {
 
     /**
      * Determines if this list is empty.
-     * - Time: O(?).
-     * - Space: O(?).
      * @returns {boolean}
      */
     isEmpty() {
@@ -52,19 +50,30 @@ class SinglyLinkedList {
     }
 
     /**
-     * Creates a new node with the given data and inserts it at the back of
-     * this list.
-     * - Time: O(?).
-     * - Space: O(?).
-     * @param {any} data The data to be added to the new node.
-     * @returns {SinglyLinkedList} This list.
+     * Creates a new node with the given data and inserts it at the back of this list.
+     * @param {any} data
+     * @returns {SinglyLinkedList}
      */
-    insertAtBack(data) {
+    insertAtBack(data, current = this.head) {
+        // Version 2 (Recursion)
+        if (!this.isEmpty()) {
+            if (current.next === null) {
+                current.next = new ListNode(data)
+                return
+            } else {
+                current = current.next
+                this.insertAtBack(data, current)
+            }
+        } else {
+            this.head = new ListNode(data)
+        }
+        /*
+        // Version 1
         // check if head is linked or not, this is needed for syntax
         if (this.isEmpty()) {
             this.head = new ListNode(data);
         } else {
-        // Otherwise we will select the head node and loop
+            // Otherwise we will select the head node and loop
             let current = this.head
             while (current.next !== null) { // current.next NOT current
                 // Update current pointer
@@ -74,8 +83,14 @@ class SinglyLinkedList {
             // create new node
             current.next = new ListNode(data)
         }
+        */
     }
 
+    /**
+    * Creates a new node with the given data and inserts that node at the front of this list.
+    * @param {any} data
+    * @returns {SinglyLinkedList}
+    */
     insertAtFront(data) {
         /* 
         Set head node to a variable
@@ -87,33 +102,112 @@ class SinglyLinkedList {
         this.head.next = headNodeVariable;
     }
 
-    removeHead () {
+    /**
+     * Removes the first node of this list.
+     * @returns {any} The data from the removed node.
+     */
+    removeHead() {
         /* 
         Determine if the head is empty
         otherwise update head with next value
          */
         if (!this.isEmpty()) {
+            let removed = this.head
             this.head = this.head.next;
+            return removed
+        }
+    }
+
+    /**
+    * Removes the last node of this list.
+    * @returns {any} The data from the node that was removed.
+    */
+    removeBack() {
+        if (!this.isEmpty()) {
+            // Check head's node exist
+            if (this.head.next !== null) {
+                let current = this.head;
+                // .next.next will check the second to last next value
+                while (current.next.next !== null) {
+                    current = current.next;
+                }
+                let removed = current.next.data;
+                current.next = null; // remove from back
+                return removed;
+            } else {
+                let removed = this.head.data;
+                this.head = null; // remove from back
+                return removed;
+            }
+        } else {
+            return null
         }
     }
 
     getNodes() {
-        const output = []
         let current = this.head
         while (current.next !== null) {
-            output.push({ node: { data: current.data } })
+            console.log(current.data)
             current = current.next
         }
-        output.push({ node: { data: current.data } })
+        console.log(current.data)
+        // const output = []
+        // let current = this.head
+        // while (current.next !== null) {
+        //     output.push({ node: { data: current.data } })
+        //     current = current.next
+        // }
+        // output.push({ node: { data: current.data } })
+        // return output
+    }
+
+
+    /**
+     * 
+     * @param {Array<any>} vals 
+     * @returns {SinglyLinkedList}
+     * insert each value in an array into SLL
+     */
+    seedFromArr(vals) {
+        // loop over val <array>
+        for (let i = 0; i < vals.length; i++) {
+            let toBeAdded = vals[i]
+            // use method to add to back (or front??)
+            this.insertAtBack(toBeAdded);
+            // this.insertAtFront(toBeAdded)
+        }
+    }
+
+
+    /**
+   * Converts this list into an array containing the data of each node.
+   * @returns {Array<any>} An array of each node's data.
+   */
+    toArr() {
+        const output = []
+        // This is our runner for going down the SLL
+        let current = this.head
+        while (current.next !== null) {
+            // push value into our array
+            output.push(current.data)
+            // Update runner
+            current = current.next;
+        }
+        // Add final value to output
+        output.push(current.data)
         return output
     }
+
 }
 
 let val = new SinglyLinkedList();
-val.insertAtFront(100);
-val.insertAtFront(50);
+val.insertAtBack(100);
+val.insertAtBack(50);
 val.insertAtBack(25);
 val.insertAtBack(5);
 val.insertAtBack(1);
-val.removeHead();
-console.log(val.getNodes())
+val.removeBack();
+// console.log(val.toArr())
+
+val.seedFromArr([1, 2, 3, 4, 5, 6, 7, 7, 109283]);
+console.log(val.getNodes());
