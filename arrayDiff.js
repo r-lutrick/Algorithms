204f46a6739d477104ffd75f1b813e33f9b532dd
Robert Lutrick <r.lutrick@icloud.com>
2023-03-17 10:12:31 -0500

    Explanation added

--- arrayDiff.js
+++ arrayDiff.js
@@ -28,3 +28,39 @@ console.log(arrayDiff([1, 2, 2], [2])); // [1]
 console.log(arrayDiff([1, 2, 2], [])); // [1, 2, 2]
 console.log(arrayDiff([], [1, 2])); // []
 console.log(arrayDiff([1, 2, 3], [1, 2])) // [3]
+
+/* 
+EXPLANATION
+There is a better way using ES7:
+
+--------INTERSECTION--------
+https://i.stack.imgur.com/uasoX.png
+
+let intersection = arr1.filter(x => arr2.includes(x));
+
+For [1,2,3] [2,3] it will yield [2,3]. 
+On the other hand, for [1,2,3] [2,3,5] will return the same thing.
+
+--------DIFFERENCE--------
+https://i.stack.imgur.com/mEtro.png
+
+let difference = arr1.filter(x => !arr2.includes(x));
+
+For [1,2,3] [2,3] it will yield [1]. 
+On the other hand, for [1,2,3] [2,3,5] will return the same thing.
+
+--------SYMMETRIC DIFFERENCE--------
+https://i.stack.imgur.com/zb1hW.png
+
+let difference = arr1
+                 .filter(x => !arr2.includes(x))
+                 .concat(arr2.filter(x => !arr1.includes(x)));
+
+This way, you will get an array containing all the elements of arr1 
+that are not in arr2 and vice-versa
+
+
+You can add this to Array.prototype so it can be used like this:
+Array.prototype.diff = function(arr2) { return this.filter(x => !arr2.includes(x)); }
+[1, 2, 3].diff([2, 3])
+*/
