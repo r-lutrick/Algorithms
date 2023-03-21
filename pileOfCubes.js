2d1d6d157e74de2cd47f16825236dd4fcd37ea78
Robert Lutrick <r.lutrick@icloud.com>
2023-03-21 15:08:35 -0500

    Initial Commit

---
+++ pileOfCubes.js
@@ -0,0 +1,10 @@
+/* 
+Your task is to construct a building which will be a pile of n cubes. 
+The cube at the bottom will have a volume of 
+(n-1)^3, the cube above will have volume of (1)^3
+
+You are given the total volume m of the building. Being given m can 
+you find the number n of cubes you will have to build?
+
+
+*/
